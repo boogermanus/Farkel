e1a82e464ceeeffa75074196bd038156354e8746
Jordan Woodruff <boogermanus@gmail.com>
2024-09-02 21:59:50 -0500

    changes to base

--- API/Dice/Dice/Models/BaseDice.cs
+++ API/Dice/Dice/Models/BaseDice.cs
@@ -2,7 +2,7 @@ using Dice.Interfaces;
 
 namespace Dice.Models;
 
-public class BaseDise<T> : IDice<T> where T : notnull
+public class BaseDice<T> : IDice<T> where T : notnull
 {
     public IEnumerable<IDie<T>> Dice => throw new NotImplementedException();
 }
---
+++ API/Dice/Dice/Models/Dice.cs
@@ -0,0 +1,6 @@
+namespace Dice.Models;
+
+public class Dice<T> : BaseDice<T> where T : notnull
+{
+
+}
